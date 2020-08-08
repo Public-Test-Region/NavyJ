20c6ef5d90f87ebec92c37d58700eb8f1fb0715c
Rémi Van Boxem <39855540+Neelty@users.noreply.github.com>
2020-08-08 14:40:17 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,5 +1,6 @@
 module.exports = {
 	name: 'avatar',
+	aliases: ['icon', 'pp'],
 	description: 'Obtenez l\'URL de l\'avatar du ou des utilisateur(s) marqué(s), ou votre propre avatar.',
 	execute(message) {
 		if (!message.mentions.users.size) {
