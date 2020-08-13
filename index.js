66887391daf0ebcddd7c81f042e879b974bd2f9d
Jules <jules.nef@orange.fr>
2020-08-13 11:55:18 +0200

    Réparation

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on('message', message => {
     }
 
     try {
-        command.execute(message, args, prefix);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply("Il y a eu une erreur en essayant d'exécuter cette commande !");
