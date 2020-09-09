7533c09645f32584476752c01f408add9d003fe9
Rémi Van Boxem <rem.vanboxem@gmail.com>
2020-09-09 21:27:46 +0200

    Rajout des dates
    Rajout de quelques dates d'anniversaire, notamment celle de Léonard Seguin ainsi que celle de Sacha Lévi

--- commands/anniv.js
+++ commands/anniv.js
@@ -7,8 +7,14 @@ module.exports = {
     if (args[0] === 'Tailongwan') {
       return message.channel.send('Louis Oudard est né le **19 Mai**. ');
     }
+    if (args[0] === 'Segronard') {
+      return message.channel.send('Léonard Seguin est né le **29 Septembre**. ');
+    }
     if (args[0] === 'foo') {
       return message.channel.send('bar');
+    }
+    if (args[0] === 'Slc') {
+      return message.channel.send('Sacha Lévi est né le **23 Janvier**. ');
     }
 
     message.channel.send(`Désolé, je ne connais pas : ${args[0]}`);
